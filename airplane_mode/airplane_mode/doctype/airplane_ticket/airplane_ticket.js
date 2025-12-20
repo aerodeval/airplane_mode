2f8179bcbcfc2d8165a5993131013a6094d1c201
Sydney <sydney223as@gmail.com>
2025-12-21 01:42:12 +0530

    feat: add dialog for assigning seats

--- airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
+++ airplane_mode/airplane_mode/doctype/airplane_ticket/airplane_ticket.js
@@ -1,8 +1,30 @@
 // Copyright (c) 2025, Sydney and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Airplane Ticket", {
-// 	refresh(frm) {
+frappe.ui.form.on("Airplane Ticket", {
+	refresh(frm) {
+        frm.add_custom_button('Assign Seat', () => {
+            let d = new frappe.ui.Dialog({
+                title: 'Select Seat',
+                fields: [
+                    {
+                        label: 'Seat Number',
+                        fieldname: 'seat_number',
+                        fieldtype: 'Data'
+                    },
+                ],
+                size: 'small', 
+                primary_action_label: 'Assign',
+                primary_action(values) {
+                    d.hide();
+                }
+            });
+            
+            d.show();
+            
+            
+            
 
-// 	},
-// });
+        }, 'Actions')
+	},
+});
