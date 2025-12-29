d9c79b6e92be9a798378a1fd9357dcc143f9dcd6
Sydney <sydney223as@gmail.com>
2025-12-29 15:08:29 +0530

    feat: update shop data on availibility of airport

--- airplane_mode/airport_shop_management/doctype/shop_details/shop_details.py
+++ airplane_mode/airport_shop_management/doctype/shop_details/shop_details.py
@@ -1,9 +1,26 @@
 # Copyright (c) 2025, Sydney and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ShopDetails(Document):
-	pass
+
+	def on_change(self):
+
+		airport = frappe.get_doc('Airport', self.airport)
+		current_shops = frappe.get_all(
+			"Shop Details",
+			filters={"Airport": airport.name},
+			fields=["Tenant"]
+		)
+
+		shop_capacity=airport.shop_capacity
+
+		# print(f'{current_shops} :current shop' )
+		# print(f'{shop_capacity} :shop' )
+		available_shops = int(shop_capacity) - len(current_shops)
+		
+		frappe.db.set_value("Airport", self.airport, "shop_availibility", available_shops)
+
