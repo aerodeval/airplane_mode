deccc19cb5b9fd5b06955dc8650a77107f8bf884
Sydney <sydney223as@gmail.com>
2025-12-29 23:20:29 +0530

    feat: add minimum rent setting while making a contract

--- airplane_mode/airport_shop_management/doctype/contract_details/contract_details.py
+++ airplane_mode/airport_shop_management/doctype/contract_details/contract_details.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2025, Sydney and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ContractDetails(Document):
-	pass
+	def before_insert(self):
+		settings = frappe.get_single("Shop Management Settings")
+		if self.rent_amount < settings.default_rent_amount:
+			frappe.throw(f"Cannot be less than default amount set by user. Must be higher than {settings.default_rent_amount}")
