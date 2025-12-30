18986b09c5dcf1d13048f02396097005ed81ec7a
Sydney <sydney223as@gmail.com>
2025-12-30 16:18:20 +0530

    feat: create a rent reciept when a contract is made

--- airplane_mode/airport_shop_management/doctype/contract_details/contract_details.py
+++ airplane_mode/airport_shop_management/doctype/contract_details/contract_details.py
@@ -6,7 +6,24 @@ from frappe.model.document import Document
 
 
 class ContractDetails(Document):
+
 	def before_insert(self):
 		settings = frappe.get_single("Shop Management Settings")
 		if self.rent_amount < settings.default_rent_amount:
 			frappe.throw(f"Cannot be less than default amount set by user. Must be higher than {settings.default_rent_amount}")
+
+	def after_insert(self):
+		self.create_rent_payment()
+
+	def create_rent_payment(self):
+		print("rent reciept created")
+		rent_doc=frappe.new_doc("Rent Payment")
+		rent_doc.contract = self.shop_name
+		rent_doc.tenant= self.tenant
+		rent_doc.data_eite = self.shop_name
+		rent_doc.payment_date = self.date_of_expiry
+		rent_doc.amount_paid = self.rent_amount
+		rent_doc.payment_status = "Pending"
+		rent_doc.insert(ignore_permissions=True)
+
+	
