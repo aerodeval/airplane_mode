025a12150d94718b3dc40478a29dd845af8f4113
Sydney <sydney223as@gmail.com>
2025-12-31 12:22:14 +0530

    feat: implement set query for shop types for filtering

--- airplane_mode/airport_shop_management/doctype/shop_type/shop_type.js
+++ airplane_mode/airport_shop_management/doctype/shop_type/shop_type.js
@@ -1,8 +1,15 @@
 // Copyright (c) 2025, Sydney and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Shop Type", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Shop Type", {
+    setup(frm) {
+        frm.set_query('shop_type',
+        () => {
+            return {
+                filters: {
+                    enabled: 1
+                }
+            };
+        });
+    }
+});
