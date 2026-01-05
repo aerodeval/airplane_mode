2128c1a4f50ac47b6568543a1f78932f7162065d
Sydney <sydney223as@gmail.com>
2026-01-05 22:22:50 +0530

    feat: add test case tp test validation message for shop capacity

--- airplane_mode/airport_shop_management/doctype/shop_details/test_shop_details.py
+++ airplane_mode/airport_shop_management/doctype/shop_details/test_shop_details.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2025, Sydney and Contributors
 # See license.txt
 
-# import frappe
+import frappe
 from frappe.tests import IntegrationTestCase
 
 
@@ -11,6 +11,16 @@ from frappe.tests import IntegrationTestCase
 EXTRA_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 IGNORE_TEST_RECORD_DEPENDENCIES = []  # eg. ["User"]
 
+airport="Test International Airport"
+
+def create_shop(shop_name, airport):
+	return frappe.get_doc({
+		"doctype": "Shop Details",
+		"shop_name": shop_name,
+		"airport": airport,
+		"shop_contact": "email",
+		"shop_type":"Stall"
+	}).insert(ignore_links=True)
 
 
 class IntegrationTestShopDetails(IntegrationTestCase):
@@ -19,4 +29,42 @@ class IntegrationTestShopDetails(IntegrationTestCase):
 	Use this class for testing interactions between multiple components.
 	"""
 
-	pass
+	def setUp(self):
+		print("setting up airport test")
+
+	def test_create_airport(self):
+			print("running test_create_airport")
+
+			return frappe.get_doc({
+				"doctype": "Airport",
+				"name": "Test International Airport",
+				"code": "TIA",
+				"city": "Test City",
+				"country": "Test Country",
+				"shop_capacity": 3
+			}).insert(ignore_permissions=True)
+
+
+	def test_create_shop(self):
+		create_shop("Shop A", airport)
+		create_shop("Shop B", airport)
+		create_shop("Shop C", airport)
+		with self.assertRaises(frappe.exceptions.ValidationError) as exc:
+			create_shop("Shop D", airport)
+
+		self.assertIn(f"Cannot add shop: Airport '{airport}' has reached its full capacity of 3 shops.", str(exc.exception))
+	
+
+
+def tearDown(self):
+    print("cleaning up airport test")
+    frappe.set_user("Administrator")
+
+    for name in frappe.get_all(
+        "Shop",
+        filters={"airport": self.airport.name},
+        pluck="name"
+    ):
+        frappe.delete_doc("Shop Details", name, force=1)
+
+    frappe.delete_doc("Airport", self.airport.name, force=1)
